b4e0b48aa50a7cce0e3a20428042ee032dcbd1c4
rjalamanac <rjalamanac@iescomercio.com>
2025-02-27 14:12:18 +0100

    Authentication stuff

--- routes/api.php
+++ routes/api.php
@@ -5,33 +5,40 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumnoController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ActividadController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::apiResource('/categorias',CategoriaController::class);
-Route::get('/categorias/{categoriaId}/actividades', 
+Route::middleware('auth:api')->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    
+    Route::apiResource('/categorias',CategoriaController::class);
+    Route::get('/categorias/{categoriaId}/actividades', 
     [CategoriaController::class, 'getActividades']);
-Route::post('/categorias/{categoriaId}/actividades/{actividadId}', 
+    Route::post('/categorias/{categoriaId}/actividades/{actividadId}', 
     [CategoriaController::class, 'associateActividad']);
-Route::delete('/categorias/{categoriaId}/actividades/{actividadId}', 
+    Route::delete('/categorias/{categoriaId}/actividades/{actividadId}', 
     [CategoriaController::class, 'disassociateActividad']);
 
-Route::apiResource('/actividades',ActividadController::class);
-Route::post('/actividades/{actividadId}/categorias/{categoriaId}', 
+    Route::apiResource('/actividades',ActividadController::class);
+    Route::post('/actividades/{actividadId}/categorias/{categoriaId}', 
     [ActividadController::class, 'associateCategoria']);
-Route::delete('/actividades/{actividadId}/categorias/{categoriaId}', 
+    Route::delete('/actividades/{actividadId}/categorias/{categoriaId}', 
     [ActividadController::class, 'disassociateCategoria']);
-Route::get('/actividades/{actividadId}/categorias', 
+    Route::get('/actividades/{actividadId}/categorias', 
     [ActividadController::class, 'getCategorias']);
-Route::get('/actividades/{actividadId}/alumnos', 
+    Route::get('/actividades/{actividadId}/alumnos', 
     [ActividadController::class, 'getAlumnos']);
 
-Route::apiResource('/alumnos',AlumnoController::class);
-Route::post('/alumnos/{alumnoId}/actividades/{actividadId}', 
+    Route::apiResource('/alumnos',AlumnoController::class);
+    Route::post('/alumnos/{alumnoId}/actividades/{actividadId}', 
     [AlumnoController::class, 'associateActividad']);
-Route::delete('/alumnos/{alumnoId}/actividades/{actividadId}', 
+    Route::delete('/alumnos/{alumnoId}/actividades/{actividadId}', 
     [AlumnoController::class, 'disassociateActividad']);
-Route::get('/alumnos/{alumnoId}/actividades', 
+    Route::get('/alumnos/{alumnoId}/actividades', 
     [AlumnoController::class, 'getActividades']);
+});
+
